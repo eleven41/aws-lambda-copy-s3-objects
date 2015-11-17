6e1be3921770ba6eee175a7085924160fd0e899f
mwhouser <matt@eleven41.com>
2015-11-16 23:32:16 -0500

    Refactor using async, add multi-bucket/region support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # An AWS Lambda Based Function to Copy S3 Objects
 
-With this AWS Lambda function, you can copy objects from a source S3 bucket to a target S3 bucket as they are added to the source bucket.
+With this AWS Lambda function, you can copy objects from a source S3 bucket to one or more target S3 buckets as they are added to the source bucket.
 
 ## Configuration
 
@@ -50,17 +50,48 @@ Create an IAM role with the following policy:
 
 ### S3 Buckets
 
-Ensure you have a source and target bucket. They do not need to reside in the same region.
+1. Ensure you have a source and target bucket. They do not need to reside in the same region.
+2. Configure your S3 buckets (see below)
 
-On the source bucket, add a tag named "TargetBucket" and give it a value of the name of the target bucket.
+### Building the Lambda Package
+
+1. Clone this repo
+
+```
+git clone git@github.com:eleven41/aws-lambda-copy-s3-objects.git
+cd aws-copy-encrypt-s3-objects
+```
+
+2. Install requirements
+
+```
+npm install async
+npm install aws-sdk
+```
+
+3. Zip up the folder using your favourite zipping utility
 
 ### Lambda Function
 
 1. Create a new Lambda function. 
-2. Upload the file `index.js` as the code for your Lambda function.
+2. Upload the ZIP package to your lambda function.
 3. Add an event source to your Lambda function:
  * Event Source Type: S3
  * Bucket: your source bucket
  * Event Type: Object Created
+4. Set your Lambda function to execute using the IAM role you created above.
 
-At this point, if you upload a file to your source bucket, the file should be copied to the target bucket.
+### Configuration
+
+Configuration is performed by setting tags on the source bucket.
+
+Tag Name | Required
+---|---
+TargetBucket | Yes
+
+**TargetBucket** - A space-separated list of buckets to which the objects will be copied. Optionally, the bucket names can contain a @ character followed by a region to indicate that the bucket resides in a different region.
+
+For example: `my-target-bucket1 my-target-bucket1@us-west-2 my-target-bucket3@us-east-1`
+
+
+At this point, if you upload a file to your source bucket, the file should be copied to the target bucket(s).
--- index.js
+++ index.js
@@ -1,57 +1,122 @@
-﻿// Load up all dependencies
+﻿console.log("Version 0.2.0");
+
+// Load up all dependencies
 var AWS = require('aws-sdk');
+var async = require('async');
 
-console.log("Version 0.1.0");
+// CreateS3
+//
+// Create a reference to an S3 client
+// in the desired region.
+function createS3(regionName) {
+    var config = { apiVersion: '2006-03-01' };
+    
+    if (regionName != null)
+        config.region = regionName;
 
-// get reference to S3 client 
-var s3 = new AWS.S3();
+    var s3 = new AWS.S3(config);
+    return s3;
+}
 
 // This is the entry-point to the Lambda function.
 exports.handler = function (event, context) {
-        
-    // Process all records in the event.
-    for (var i = 0; i < event.Records.length; ++i) {
-        
-        // The source bucket and source key are part of the event data
-        var srcBucket = event.Records[i].s3.bucket.name;
-        var srcKey = unescape(event.Records[i].s3.object.key);
-        
-        // Get the target bucket based on the source bucket.
-        // Once we have that, perform the copy.
-        getTargetBucket(srcBucket, function (targetBucket) {
-            console.log("Copying '" + srcKey + "' from '" + srcBucket + "' to '" + targetBucket + "'");
-
-            var dstBucket = targetBucket;
-            var dstKey = srcKey;
-            
-            // Copy the object from the source bucket
-            s3.copyObject({
-                Bucket: dstBucket,
-                Key: dstKey,
-                
-                CopySource: escape(srcBucket + '/' + srcKey),
-                MetadataDirective: 'COPY'
-            }, function (err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else console.log(data);           // successful response
-            });
-        });
+    
+    if (event.Records == null) {
+        context.fail('Error', "Event has no records.");
+        return;
     }
+    
+    // Process all records in the event asynchronously.
+    async.each(event.Records, processRecord, function (err) {
+        if (err) {
+            context.fail('Error', "One or more objects could not be copied.");
+        } else {
+            context.succeed();
+        }
+    });
 };
 
-// getTargetBucket
+// processRecord
+//
+// Iterator function for async.each (called by the handler above).
+//
+// 1. Get the target bucket from the source bucket's tags
+// 2. Copy the object to each of the desired buckets.
+function processRecord(record, callback) {
+        
+    // The source bucket and source key are part of the event data
+    var srcBucket = record.s3.bucket.name;
+    var srcKey = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+        
+    // Get the target bucket(s) based on the source bucket.
+    // Once we have that, perform the copy.
+    getTargetBuckets(srcBucket, function (err, targets) {
+        if (err) {
+            console.log("Error getting target bucket: "); // an error occurred
+            console.log(err, err.stack); // an error occurred
+            callback("Error getting target bucket from source bucket '" + srcBucket + "'");
+            return;
+        }
+        
+        async.each(targets, function (target, callback) {
+
+            var targetBucket = target.bucketName;
+            var regionName = target.regionName;
+            var targetBucketName = targetBucket;
+            if (regionName != null)
+                targetBucketName = targetBucketName + "@" + regionName;
+            var targetKey = srcKey;
+            
+            console.log("Copying '" + srcKey + "' from '" + srcBucket + "' to '" + targetBucketName + "'");
+            
+            // Copy the object from the source bucket
+            var s3 = createS3(regionName);
+            s3.copyObject({
+                Bucket: targetBucket,
+                Key: targetKey,
+                
+                CopySource: encodeURIComponent(srcBucket + '/' + srcKey),
+                MetadataDirective: 'COPY'
+            }, function (err, data) {
+                if (err) {
+                    console.log("Error copying '" + srcKey + "' from '" + srcBucket + "' to '" + targetBucketName + "'");
+                    console.log(err, err.stack); // an error occurred
+                    callback("Error copying '" + srcKey + "' from '" + srcBucket + "' to '" + targetBucketName + "'");
+                } else {
+                    callback();
+                }
+            });
+        }, function (err) {
+            if (err) {
+                callback(err);
+            } else {
+                callback();
+            }
+        });
+    });
+};
+
+// getTargetBuckets
 //
 // Gets the tags for the named bucket, and
 // from those tags, finds the "TargetBucket" tag.
 // Once found, it calls the callback function passing
 // the tag value as the single parameter.
-function getTargetBucket(bucketName, callback) {
+function getTargetBuckets(bucketName, callback) {
     console.log("Getting tags for bucket '" + bucketName + "'");
+    
+    var s3 = createS3();
     s3.getBucketTagging({
         Bucket: bucketName
     }, function (err, data) {
         if (err) {
-            console.log(err, err.stack); // an error occurred
+            if (err.code == 'NoSuchTagSet') {
+                // No tags on the bucket, so the bucket is not configured properly.
+                callback("Source bucket '" + bucketName + "' is missing 'TargetBucket' tag.", null);
+            } else {
+                // Some other error
+                callback(err, null);
+            }
             return;
         }
         
@@ -63,11 +128,30 @@ function getTargetBucket(bucketName, callback) {
             var tag = tags[i];
             if (tag.Key == 'TargetBucket') {
                 console.log("Tag 'TargetBucket' found with value '" + tag.Value + "'");
-                callback(tag.Value);
+                
+                var tagValue = tag.Value.trim();
+                var buckets = tag.Value.split(' ');
+                
+                var targets = [];
+                
+                for (var i = 0; i < buckets.length; ++i) {
+                    var bucketSpec = buckets[i].trim();
+                    if (bucketSpec.length == 0)
+                        continue;
+                    
+                    var specParts = bucketSpec.split('@');
+                    
+                    var bucketName = specParts[0];
+                    var regionName = specParts[1]
+
+                    targets.push({ bucketName: bucketName, regionName: regionName });
+                }
+                
+                callback(null, targets);
                 return;
             }
         }
         
-        console.log("Tag 'TargetBucket' not found");
+        callback("Tag 'TargetBucket' not found", null);
     });
 }
